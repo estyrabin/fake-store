80863481a305cd38322b2011696fc10efd7a885e
estyrabin <estyrabi@gmail.com>
2025-09-21 14:52:08 +0300

    update

--- src/services/store.js
+++ src/services/store.js
@@ -1,18 +1,19 @@
+import { getFromStorage, setToStorage } from "../utils/utils";
+
 export const getProducts = async () => {
   try {
-    const cached = localStorage.getItem("products");
+    const cached = getFromStorage("products");
 
     if (cached) {
-      return JSON.parse(cached);
+      return cached;
     }
 
     const response = await fetch("https://fakestoreapi.com/products");
     const data = await response.json();
 
-    localStorage.setItem("products", JSON.stringify(data));
+    setToStorage("products", data);
 
     return data;
-    
   } catch (error) {
     console.error(`Error: ${error}`);
     return [];
